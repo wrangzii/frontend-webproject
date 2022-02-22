c39454baab1ee938d0d2b43e07a8ac1fb558431d
SPLUS\KhanhTQ <khanhtq@splus-software.com.vn>
2022-02-23 00:35:42 +0700

    push code test bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Register from './components/admin/Register';
 
 import {
   BrowserRouter,
-  Switch,
+  Routes,
   Route,
 } from "react-router-dom";
 import ListUser from './components/users/ListUser';
@@ -19,7 +19,6 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="authentication container">
-<<<<<<< HEAD
           <Routes>
             <Route exact path='/' element={<Login />} />
             <Route exact path='/forgot' element={<ForgotPassword />} />
@@ -29,13 +28,6 @@ function App() {
             <Route exact path='/listuser/edit/:id' element={<EditUser />} />
             <Route exact path='/listuser/:id' element={<ViewUser />} />
           </Routes>
-=======
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path='/forgot' component={ForgotPassword} />
-            <Route path='/register' component={Register} />
-          </Switch>
->>>>>>> 1829933245668452961f353126f7598bc6e414b5
         </div>
       </div>
     </BrowserRouter>
