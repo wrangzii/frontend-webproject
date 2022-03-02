3756e4e21e479b5c15ff0f1b3d996f5555befe4c
SPLUS\KhanhTQ <khanhtq@splus-software.com.vn>
2022-03-02 23:24:34 +0700

    Add user

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -3,105 +3,242 @@ import axios from "axios";
 
 import { Link, useNavigate } from "react-router-dom";
 
-const AddUser = () => {
+export default class AddUser extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            "email": "",
+            "username": "",
+            "fullname": "",
+            "phoneNumber": "",
+            "dateOfBirth": "",
+        }
+    }
 
-    let navigate = useNavigate();
-    const [user, setUser] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        department: "",
-        role: ""
-    });
+    setParams = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
 
-    const { name, email, phone, department, role } = user;
-    const onInputChange = e => {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    };
+    addUser = () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+            "email": this.state.username,
+            "username": this.state.username,
+            "fullname": this.state.fullname,
+            "phoneNumber": this.state.phoneNumber,
+            "dateOfBirth": this.state.dateOfBirth,
+        });
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: './ListUser.js'
+        };
+        fetch("http://localhost:8080/collecting_idea/user", requestOptions)
+            .then(response => {
+                console.log(response);
+                if (response.ok) {
+                    return response.json()
+                }
 
-    const onSubmit = async e => {
-        e.preventDefault();
-        await axios.post("http://localhost:3003/users", user);
-        navigate("/users");
-    };
+                throw Error(response.status)
+            })
+            .then(result => {
+                console.log(result)
+                localStorage.setItem("token", result.token)
+                console.log(result.username)
+                alert(localStorage.getItem('token'))
+                axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token')
+            })
+            .catch(error => {
+                console.log('error', error)
+                alert("Wrong")
+            });
+    }
 
-    return (
-        <div className="col-12 col-md-9 col-lg-6 mx-auto shadow p-3 p-md-5">
-            <h2 className="text-center mb-4">Add New User</h2>
-            <form onSubmit={e => onSubmit(e)}>
-                <div className="form-group">
-                    <label htmlFor="name">Full Name</label>
-                    <input
-                        type="text"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Your Name"
-                        name="name"
-                        value={name}
-                        onChange={e => onInputChange(e)}
-                    />
+    render() {
+        return (
+            <>
+                <div className="col-12 col-md-9 col-lg-6 mx-auto shadow p-3 p-md-5">
+                    <h2 className="text-center mb-4">Add New User</h2>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name">Full Name</label>
+                            <input
+                                type="text"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Your Name"
+                                name="name"
+                                value={this.username}
+                                onChange={this.setParams}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Your E-mail Address"
+                                name="email"
+                                value={this.email}
+                                onChange={this.setParams}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="phone">Phone No.</label>
+                            <input
+                                type="text"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Your Phone Number"
+                                name="phone"
+                                value={this.phoneNumber}
+                                onChange={this.setParams}
+                            />
+                        </div>
+                        {/* <div className="form-group">
+                            <label htmlFor="department">Department</label>
+                            <select
+                                name="department"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Your Department"
+                                value={this.department}
+                                onChange={this.setParams}
+                            >
+                                <optgroup label="Department">
+                                    <option value="QA department">QA department</option>
+                                    <option value="Falcuty of IT">Falcuty of IT</option>
+                                    <option value="HR department">HR department</option>
+                                </optgroup>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="role">Role</label>
+                            <select
+                                name="role"
+                                className="form-control form-control-lg"
+                                placeholder="Enter Your Role"
+                                value={role}
+                                onChange={this.setParams}
+                            >
+                                <optgroup label="Role">
+                                    <option value="Admin">Admin</option>
+                                    <option value="QA manager">QA manager</option>
+                                    <option value="QA coordinator">QA coordinator</option>
+                                    <option value="Staff">Staff</option>
+                                </optgroup>
+                            </select>
+                        </div> */}
+                        <div className="form-group text-right">
+                            <button className="btn btn-primary px-3 mr-3" onClick={this.addUser}>Add User</button>
+                            <Link to="/users" className="btn btn-danger px-3">Cancel</Link>
+                        </div>
+                    </form>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Your E-mail Address"
-                        name="email"
-                        value={email}
-                        onChange={e => onInputChange(e)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="phone">Phone No.</label>
-                    <input
-                        type="text"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Your Phone Number"
-                        name="phone"
-                        value={phone}
-                        onChange={e => onInputChange(e)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="department">Department</label>
-                    <select
-                        name="department"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Your Department"
-                        value={department}
-                        onChange={e => onInputChange(e)}
-                    >
-                        <optgroup label="Department">
-                            <option value="QA department">QA department</option>
-                            <option value="Falcuty of IT">Falcuty of IT</option>
-                            <option value="HR department">HR department</option>
-                        </optgroup>
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="role">Role</label>
-                    <select
-                        name="role"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Your Role"
-                        value={role}
-                        onChange={e => onInputChange(e)}
-                    >
-                        <optgroup label="Role">
-                            <option value="Admin">Admin</option>
-                            <option value="QA manager">QA manager</option>
-                            <option value="QA coordinator">QA coordinator</option>
-                            <option value="Staff">Staff</option>
-                        </optgroup>
-                    </select>
-                </div>
-                <div className="form-group text-right">
-                    <button className="btn btn-primary px-3 mr-3">Add User</button>
-                    <Link to="/users" className="btn btn-danger px-3">Cancel</Link>
-                </div>
-            </form>
-        </div>
-    )
+            </>
+        )
+    }
 }
 
-export default AddUser;
+// const AddUser = () => {
+
+//     let navigate = useNavigate();
+//     const [user, setUser] = useState({
+//         name: "",
+//         email: "",
+//         phone: "",
+//         department: "",
+//         role: ""
+//     });
+
+//     const { name, email, phone, department, role } = user;
+//     const onInputChange = e => {
+//         setUser({ ...user, [e.target.name]: e.target.value });
+//     };
+
+//     const onSubmit = async e => {
+//         e.preventDefault();
+//         await axios.post("http://localhost:3003/users", user);
+//         navigate("/users");
+//     };
+
+//     return (
+        // <div className="col-12 col-md-9 col-lg-6 mx-auto shadow p-3 p-md-5">
+        //     <h2 className="text-center mb-4">Add New User</h2>
+        //     <form onSubmit={e => onSubmit(e)}>
+        //         <div className="form-group">
+        //             <label htmlFor="name">Full Name</label>
+        //             <input
+        //                 type="text"
+        //                 className="form-control form-control-lg"
+        //                 placeholder="Enter Your Name"
+        //                 name="name"
+        //                 value={name}
+        //                 onChange={e => onInputChange(e)}
+        //             />
+        //         </div>
+        //         <div className="form-group">
+        //             <label htmlFor="email">Email</label>
+        //             <input
+        //                 type="email"
+        //                 className="form-control form-control-lg"
+        //                 placeholder="Enter Your E-mail Address"
+        //                 name="email"
+        //                 value={email}
+        //                 onChange={e => onInputChange(e)}
+        //             />
+        //         </div>
+        //         <div className="form-group">
+        //             <label htmlFor="phone">Phone No.</label>
+        //             <input
+        //                 type="text"
+        //                 className="form-control form-control-lg"
+        //                 placeholder="Enter Your Phone Number"
+        //                 name="phone"
+        //                 value={phone}
+        //                 onChange={e => onInputChange(e)}
+        //             />
+        //         </div>
+        //         <div className="form-group">
+        //             <label htmlFor="department">Department</label>
+        //             <select
+        //                 name="department"
+        //                 className="form-control form-control-lg"
+        //                 placeholder="Enter Your Department"
+        //                 value={department}
+        //                 onChange={e => onInputChange(e)}
+        //             >
+        //                 <optgroup label="Department">
+        //                     <option value="QA department">QA department</option>
+        //                     <option value="Falcuty of IT">Falcuty of IT</option>
+        //                     <option value="HR department">HR department</option>
+        //                 </optgroup>
+        //             </select>
+        //         </div>
+        //         <div className="form-group">
+        //             <label htmlFor="role">Role</label>
+        //             <select
+        //                 name="role"
+        //                 className="form-control form-control-lg"
+        //                 placeholder="Enter Your Role"
+        //                 value={role}
+        //                 onChange={e => onInputChange(e)}
+        //             >
+        //                 <optgroup label="Role">
+        //                     <option value="Admin">Admin</option>
+        //                     <option value="QA manager">QA manager</option>
+        //                     <option value="QA coordinator">QA coordinator</option>
+        //                     <option value="Staff">Staff</option>
+        //                 </optgroup>
+        //             </select>
+        //         </div>
+        //         <div className="form-group text-right">
+        //             <button className="btn btn-primary px-3 mr-3">Add User</button>
+        //             <Link to="/users" className="btn btn-danger px-3">Cancel</Link>
+        //         </div>
+        //     </form>
+        // </div>
+//     )
+// }
+
+// export default AddUser;
