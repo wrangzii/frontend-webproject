67e64c6be2307ab4d018d077657fc0e802b0c8d6
ManhHai <haivmgcs18615@fpt.edu.vn>
2022-03-10 13:08:06 +0700

    rebuild list and view department page

--- src/components/departments/ListDepart.js
+++ src/components/departments/ListDepart.js
@@ -3,59 +3,132 @@ import { Link } from "react-router-dom";
 
 import axios from "axios";
 
-const ListDepart = () => {
-
-    const [departs, setDepart] = useState([]);
-
-    useEffect(() => {
-        loadDeparts();
-    }, []);
-
-    const loadDeparts = async () => {
-        const result = await axios.get("http://localhost:8080/departs");
-        setDepart(result.data);
+export default class ListDepart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            departs: [],
+        };
     }
 
-    const deleteDepart = async id => {
-        await axios.delete(`http://localhost:8080/departs/${id}`);
-        loadDeparts();
+    listDepart() {
+        fetch("http://localhost:8080/departs")
+        .then((res) => res.json())
+        .then((json) => {
+            this.setState({
+                departs: json
+            });
+        })
     }
 
-    return (
-        <div className="list-depart">
+    deleteDepart() {
+        fetch(`http://localhost:8080/departs/${id}`, {
+            method: 'DELETE'
+        });
+    }
 
-            <Link className="btn btn-outline-dark mb-3" to="/departments/add">Add Department</Link>
+    render() {
 
-            <div className="overflow-auto">
-                <table className="table border shadow">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Description</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            departs.map((depart, index) => (
-                                <tr key={index}>
-                                    <th scope="row">{index + 1}</th>
-                                    <td>{depart.name}</td>
-                                    <td>{depart.description}</td>
-                                    <td>
-                                        <Link className="btn btn-primary mr-2" to={`/departments/${depart.id}`}>View</Link>
-                                        <Link className="btn btn-outline-primary mr-2" to={`/departments/edit/${depart.id}`}>Edit</Link>
-                                        <Link className="btn btn-outline-danger mr-2" onClick={() => deleteDepart(depart.id)} to="/departments">Delete</Link>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+        useEffect(() => {
+            listDepart()
+        }, [])
+
+        const { departs } = this.state;
+
+        return (
+            <>
+            <div className="list-depart">
+
+                <Link className="btn btn-outline-dark mb-3" to="/departments/add">Add Department</Link>
+
+                <div className="overflow-auto">
+                    <table className="table border shadow">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">departmentName</th>
+                                {/* <th scope="col">Description</th> */}
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                departs.map((depart, index) => (
+                                    <tr key={index}>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>{depart.departmentName}</td>
+                                        {/* <td>{depart.description}</td> */}
+                                        <td>
+                                            <Link className="btn btn-primary mr-2" to={`/departments/${depart.id}`}>View</Link>
+                                            <Link className="btn btn-outline-primary mr-2" to={`/departments/edit/${depart.id}`}>Edit</Link>
+                                            <Link className="btn btn-outline-danger mr-2" onClick={() => deleteDepart(depart.id)} to="/departments">Delete</Link>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    );
+            </>
+        );
+    }
 }
 
-export default ListDepart;
+
+// const ListDepart = () => {
+
+//     const [departs, setDepart] = useState([]);
+
+//     useEffect(() => {
+//         loadDeparts();
+//     }, []);
+
+//     const loadDeparts = async () => {
+//         const result = await axios.get("http://localhost:8080/departs");
+//         setDepart(result.data);
+//     }
+
+    // const deleteDepart = async id => {
+    //     await axios.delete(`http://localhost:8080/departs/${id}`);
+    //     loadDeparts();
+    // }
+
+//     return (
+        // <div className="list-depart">
+
+        //     <Link className="btn btn-outline-dark mb-3" to="/departments/add">Add Department</Link>
+
+        //     <div className="overflow-auto">
+        //         <table className="table border shadow">
+        //             <thead>
+        //                 <tr>
+        //                     <th scope="col">ID</th>
+        //                     <th scope="col">Name</th>
+        //                     <th scope="col">Description</th>
+        //                     <th>Action</th>
+        //                 </tr>
+        //             </thead>
+        //             <tbody>
+        //                 {
+        //                     departs.map((depart, index) => (
+        //                         <tr key={index}>
+        //                             <th scope="row">{index + 1}</th>
+        //                             <td>{depart.name}</td>
+        //                             <td>{depart.description}</td>
+        //                             <td>
+        //                                 <Link className="btn btn-primary mr-2" to={`/departments/${depart.id}`}>View</Link>
+        //                                 <Link className="btn btn-outline-primary mr-2" to={`/departments/edit/${depart.id}`}>Edit</Link>
+        //                                 <Link className="btn btn-outline-danger mr-2" onClick={() => deleteDepart(depart.id)} to="/departments">Delete</Link>
+        //                             </td>
+        //                         </tr>
+        //                     ))
+        //                 }
+        //             </tbody>
+        //         </table>
+        //     </div>
+        // </div>
+//     );
+// }
+
+// export default ListDepart;
--- src/components/departments/ViewDepart.js
+++ src/components/departments/ViewDepart.js
@@ -2,35 +2,68 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-const ViewDepart = () => {
+export default class ListDepart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            departs: [],
+        };
+    }
 
-    const [depart, setDepart] = useState({
-        name: "",
-        description: "",
-    });
+    viewDepart() {
+        fetch(`http://localhost:8080/departs/${id}`)
+        .then((res) => res.json())
+        .then((json) => {
+            this.setState({
+                departs: json
+            });
+        })
+    }
 
-    const { id } = useParams();
-
-    useEffect(() => {
-        loadDepart();
-    }, []);
-
-    const loadDepart = async () => {
-        const res = await axios.get(`http://localhost:8080/departs/${id}`);
-        setDepart(res.data);
-    };
-
-    return (
-        <>
+    render() {
+        return (
+            <>
             <Link className="btn btn-primary" to="/departments">List Department</Link>
             <h3 className="display-4">Department Id: {id}</h3>
-            <hr />
-            <ul className="list-group col-12 col-md-9 col-lg-6 px-0">
-                <li className="list-group-item text-break">Name: {depart.name}</li>
-                <li className="list-group-item text-break">Description: {depart.description}</li>
+               <hr />
+             <ul className="list-group col-12 col-md-9 col-lg-6 px-0">
+                <li className="list-group-item text-break">Department Name: {departs.departmentName}</li>
+                {/* <li className="list-group-item text-break">Description: {departs.description}</li> */}
             </ul>
-        </>
-    );
-};
+            </>
+        )
+    }
+}
 
-export default ViewDepart;
+// const ViewDepart = () => {
+
+//     const [depart, setDepart] = useState({
+//         name: "",
+//         description: "",
+//     });
+
+//     const { id } = useParams();
+
+//     useEffect(() => {
+//         loadDepart();
+//     }, []);
+
+//     const loadDepart = async () => {
+//         const res = await axios.get(`http://localhost:8080/departs/${id}`);
+//         setDepart(res.data);
+//     };
+
+//     return (
+//         <>
+//             <Link className="btn btn-primary" to="/departments">List Department</Link>
+//             <h3 className="display-4">Department Id: {id}</h3>
+//             <hr />
+//             <ul className="list-group col-12 col-md-9 col-lg-6 px-0">
+//                 <li className="list-group-item text-break">Name: {depart.name}</li>
+//                 <li className="list-group-item text-break">Description: {depart.description}</li>
+//             </ul>
+//         </>
+//     );
+// };
+
+// export default ViewDepart;
