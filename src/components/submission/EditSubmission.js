a6640ae898d71845eb1dae2ae610d8081086e145
ManhHai <haivmgcs18615@fpt.edu.vn>
2022-03-17 18:09:50 +0700

    edit and view submission

--- src/components/submission/EditSubmission.js
+++ src/components/submission/EditSubmission.js
@@ -1,9 +1,119 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Cookies } from "react-cookie";
+import { useParams } from "react-router-dom";
 
-function EditSubmission() {
+const EditSubmission = () => {
+    const { id } = useParams();
+    const [submissionName, setsubmissionName] = useState("");
+    const [description, setDescription] = useState("");
+    const [closureDate, setClosureDate] = useState("");
+    const [finalClosureDate, setFinalClosureDate] = useState("");
+    // const [message, setMessage] = useState("")
+    const navigate = useNavigate()
+    const editSubmission = () => {
+        const cookies = new Cookies();
+
+        const raw = JSON.stringify({
+            submissionName,
+            description,
+            closureDate,
+            finalClosureDate
+        });
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                'Authorization': 'Bearer ' + cookies.get('token'),
+                'Content-Type': 'application/json'
+            },
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch(`http://localhost:8080/submission/edit/${id}`, requestOptions)
+            .then(response => {
+                if (response.ok)
+                    response.json()
+                throw Error(checkError())
+            })
+            .then(result => {
+                // setMessage(result.message)
+                console.log(result);
+                navigate('/submission')
+            })
+            .catch(error => createAlert(error))
+
+    }
+
+    function checkError() {
+        let msg = ""
+        if (document.querySelector("input[type=text]").value === "") {
+            msg = "Not allow blank"
+        } else {
+            msg = "Submission name is exist"
+        }
+        return msg
+    }
+
+    function createAlert(message) {
+        const title = document.querySelector("h3")
+        const alert = document.createElement("p")
+        if (!document.querySelector(".alert-danger")) {
+            title.after(alert)
+        } else {
+            document.querySelector(".alert-danger").remove()
+            title.after(alert)
+        }
+
+        alert.textContent = message
+        alert.setAttribute("class", "alert alert-danger")
+    }
+
     return (
-        <div>EditSubmission</div>
-    )
-}
-
-export default EditSubmission
+        <div className="col-12 col-md-9 col-lg-6 mx-auto shadow p-3 p-md-5">
+            <h3 className="text-center mb-4">Edit Submission</h3>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="submission-name">Submission Name</label>
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Enter Submission"
+                        name="submissiontName"
+                        value={submissionName}
+                        onChange={e => setsubmissionName(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="Enter Description"
+                        name="description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        className="form-control form-control-lg"
+                        placeholder="Enter ClosureDate"
+                        name="closureDate"
+                        value={closureDate}
+                        onChange={e => setClosureDate(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        className="form-control form-control-lg"
+                        placeholder="Enter FinalClosureDate"
+                        name="finalClosureDate"
+                        value={finalClosureDate}
+                        onChange={e => setFinalClosureDate(e.target.value)}
+                    />
+                </div>
+                <div className="form-group text-right">
+                    <button type="button" className="btn btn-warning px-3 mr-3" onClick={editSubmission}>Update</button>
+                    <Link to="/submission" className="btn btn-danger px-3">Cancel</Link>
+                </div>
+            </form>
+        </div>
+    );
+};
+export default EditSubmission;
--- src/components/submission/ViewSubmission.js
+++ src/components/submission/ViewSubmission.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Cookies } from "react-cookie";
 
-function ViewSubmission() {
+const ViewSubmission = () => {
+    const { id } = useParams();
+    const [submissions, setsubmissions] = useState([])
+
+    const cookies = new Cookies();
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + cookies.get('token'),
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow'
+    };
+    useEffect(() => {
+        fetch(`http://localhost:8080/submission/${id}`, requestOptions)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw Error(response.message);
+            })
+            .then(result => setsubmissions(result.data))
+            .catch(error => {
+                alert(error)
+            });
+    }, [])
+
     return (
-        <div>ViewSubmission</div>
-    )
-}
+        <div className="view-submission">
+            <Link className="btn btn-primary" to="/submission">List Submission</Link>
+            <h3 className="font-weight-bold mt-3">Submission ID: #{submissions.submissionNameId}</h3>
+            <hr />
+            <ul className="list-group col-12 col-md-9 col-lg-6 px-0">
+                <li className="list-group-item text-break">Submission Name: {submissions.submissionName}</li>
+                <li className="list-group-item text-break">Description: {submissions.description}</li>
+                <li className="list-group-item text-break">Closure Date: {submissions.closureDate}</li>
+                <li className="list-group-item text-break">Final Closure Date: {submissions.finalClosureDate}</li>
+            </ul>
+        </div>
+    );
+};
 
-export default ViewSubmission
+export default ViewSubmission;
